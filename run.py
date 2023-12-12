2bc4bd964584e78ae0d3c1dfc8e350f94f88d321
witek <witek.nowogorski@gmail.com>
2023-12-13 00:49:35 +0100

    .

--- run.py
+++ run.py
@@ -18,9 +18,11 @@ def get_response(query):
     docs = docsearch.similarity_search(query)
     print(docs)
     template = PromptTemplate.from_template(
-        "You are given a texts and a query. Texts are trasncriptions of youtube interview video. You need to answer the query on the basis of texts. Try to answer only based on these texts, if it's impossible, answer that: \"This interview doesn't contain this information \" \n\n Paragraph:\n{info} \n Query:\n {query}"
+        "You are given a texts and a query. Texts are trasncriptions of youtube interview video."
+        " You need to answer the query on the basis of texts. Try to answer only based on these texts, "
+        "if it's impossible, answer something like that: \"This interview doesn't contain this information \" \n\n Paragraph:\n{info} \n Query:\n {query}"
     )
-    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=1, openai_api_key=config['open_ai_api_key'])
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=1.5, openai_api_key=config['open_ai_api_key'])
     chain = LLMChain(
         llm=llm,
         prompt=template,
@@ -44,4 +46,4 @@ def run():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
